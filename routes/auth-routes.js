34b85c8e099512b5635075d2b5718d9e17de92fa
unknown <shabankhawar99@gmail.com>
2020-01-05 00:09:54 +0500

    more basic setup

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,7 +1,26 @@
 const router = require("express").Router();
+const passport = require("passport");
 
 router.get("/", (req, res) => {
   res.send("you are here login");
+});
+
+//login route
+router.get("/login", (req, res) => {
+  res.send("IM HERE NOW!!!");
+});
+
+// sign up route
+router.get("/signup", (req, res) => {
+  res.send("IM DOING THE BEST I CANNN");
+});
+
+// auth with google
+router.get("/google", passport.authenticate("google"));
+
+//sign out
+router.get("/logout", (req, res) => {
+  res.send("ARE YOU NOW??!?!?!?!??!?!");
 });
 
 module.exports = router;
