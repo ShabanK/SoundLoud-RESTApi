39f73af3a0a49e60eee5c87c07eaece60700cb85
unknown <shabankhawar99@gmail.com>
2020-01-06 03:05:39 +0500

    set up cookies

--- config/passportauth.js
+++ config/passportauth.js
@@ -3,7 +3,14 @@ require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth2").Strategy;
 const User = require("../models/userSchema");
-const mongoose = require("mongoose");
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => done(null, user));
+});
 
 passport.use(
   new GoogleStrategy(
@@ -17,11 +24,13 @@ passport.use(
       User.findOne({ googleId: profile.id }).then(currentUser => {
         if (currentUser) {
           console.log(`user is ${currentUser}`);
+          done(null, currentUser);
         } else {
           new User({ username: profile.displayName, googleId: profile.id })
             .save()
             .then(newUser => {
               console.log(newUser);
+              done(null, newUser);
             });
         }
       });
--- server.js
+++ server.js
@@ -6,13 +6,25 @@ const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 const authRoutes = require("./routes/auth-routes");
 const song = require("./routes/songs/song");
+const passport = require("passport");
 const passportSetup = require("./config/passportauth");
+const cookieSession = require("cookie-session");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(
+  cookieSession({
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [process.env.cookieKey]
+  })
+);
+//initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
 
+//connect to db
 const db = mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
